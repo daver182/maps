99bcd1a4c81f0643ed2ed2eb2ccc74477ac07143
Daniel Vergara Urrutia <daver182@gmail.com>
2015-08-21 13:12:05 -0300

    Version inicial, se agrego el mapa, la busqueda de una ubicacion y los marcadores

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,23 +8,24 @@
  *
  * Main module of the application.
  */
-angular
-  .module('mapsApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('mapsApp', [ 'ngRoute', 'uiGmapgoogle-maps' ]).config(function ($routeProvider, uiGmapGoogleMapApiProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl',
+				controllerAs: 'main'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutCtrl',
+				controllerAs: 'about'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		uiGmapGoogleMapApiProvider.configure({
+			key: 'AIzaSyBOuZ5mjKKatmN6x8zdCQoT8ZXrdYnwCi4',
+			v: '3.17',
+			libraries: 'places'
+		});
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,98 @@
  * # MainCtrl
  * Controller of the mapsApp
  */
-angular.module('mapsApp').controller('MainCtrl', function () {
-    
+angular.module('mapsApp').controller('MainCtrl', function ($scope) {
+    $scope.locations = [];
+
+    $scope.map = {
+    	center: {
+    		latitude: -33.4,
+    		longitude:  -70.65
+    	},
+    	zoom: 12,
+    	options: { scrollwheel: false }
+    };
+    $scope.marker = {
+    	id: 1,
+		options: { draggable: true },
+		events: { dragend: draggedMarker }
+	};
+
+    $scope.search = {
+    	template: 'views/search.html',
+    	events: { place_changed: placeChanged },
+    	parent: 'search',
+    	options: {
+    		autocomplete: true
+    	}
+    };
+
+    $scope.save = function(){
+    	if(!$scope.lastLocation.coords) return;
+
+    	$scope.locations.push({
+    		id: Date.now(),
+    		latitude: $scope.lastLocation.coords.latitude,
+    		longitude: $scope.lastLocation.coords.longitude,
+    		address: $scope.lastLocation.address
+    	});
+
+    	$scope.marker = {};
+        $scope.lastLocation = {};
+    }
+
+    $scope.show = function(location){
+    	$scope.map.center = {
+    		latitude: location.latitude,
+    		longitude: location.longitude,
+    	};
+    }
+
+    $scope.delete = function(index){
+    	$scope.locations.splice(index, 1);
+    }
+
+    function draggedMarker (marker, eventName, args) {
+    	var geocoder = new google.maps.Geocoder();
+    	var latitude = marker.getPosition().lat();
+		var longitude = marker.getPosition().lng();
+
+    	var latlng = { lat: latitude, lng: longitude };
+    	geocoder.geocode({ 'location': latlng }, function(results, status) {
+    		if (status === google.maps.GeocoderStatus.OK) {
+    			if (results[1]) {
+    				$scope.lastLocation = {
+    					coords: {
+    						latitude: latitude,
+							longitude: longitude
+    					},
+						address: results[1].formatted_address
+					}
+    			}else {
+					console.error('No se encontró la dirección');
+				}
+    		} else {
+				console.error('Geocoder ha fallado. Error: ' + status);
+			}
+    	});
+	}
+
+	function placeChanged (autocomplete) {
+		var location = autocomplete.getPlace().geometry.location;
+
+		if(!location) return;
+
+		var coords = {
+			latitude: location.k,
+			longitude: location.D
+		}
+
+		$scope.map.center = coords;
+		$scope.marker.coords = coords;
+
+		$scope.lastLocation = {
+			coords: coords,
+			address: autocomplete.getPlace().formatted_address
+		}
+	}
 });
