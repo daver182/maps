eb5d1709649bcab4b418736737f444f308f749fb
Daniel Vergara Urrutia <daver182@gmail.com>
2015-08-21 13:02:04 -0300

    Commit inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mapsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mapsApp
+ */
+angular.module('mapsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mapsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mapsApp
+ */
+angular.module('mapsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
